20f164df72b3cf1ea0d8ced9ca4dd1bac71475a2
Christopher-Villapuda <49170925+Christopher-Villapuda@users.noreply.github.com>
2020-09-14 15:21:05 -0500

    Movement
    Added Horizontal Movement

---
+++ HGP/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 15;
+    private float xRange = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+         }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+    }
+}
