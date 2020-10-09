547c1acde64436201cd5a36ab1150535bfff08fd
LittleFiery1 <47900497+LittleFiery1@users.noreply.github.com>
2020-10-09 13:54:06 -0500

    Wrote a script for the Enemy to follow the player. Also rewrote the player movement script.

---
+++ HGP/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    private Vector2 targetVector;
+    private Rigidbody2D rBD2D;
+    //private Vector2 enemy;
+    [SerializeField]
+    private float speed = 10.0f;
+
+    void Start()
+    {
+        //targetVector = target.position;
+        //enemy = transform.position;
+        rBD2D = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        float step = speed * Time.deltaTime;
+        targetVector = target.position;
+
+        rBD2D.MovePosition(Vector2.MoveTowards(transform.position, targetVector, step));
+    }
+}
