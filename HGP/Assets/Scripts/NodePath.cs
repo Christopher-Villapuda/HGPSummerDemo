946ab99dc83ae06f48cd6c6486e4e2784abbbac0
LittleFiery1 <47900497+LittleFiery1@users.noreply.github.com>
2020-11-09 20:48:44 -0600

    Basic Nodes code

--- HGP/Assets/Scripts/NodePath.cs
+++ HGP/Assets/Scripts/NodePath.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class NodePath : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    List<GameObject> nodes;
+    float speed = 5.0f;
+    int nodeCount = 0;
 
-    void Update()
+    void FixedUpdate()
     {
-        
+        float step = speed * Time.deltaTime;
+        transform.position = Vector2.MoveTowards(transform.position, nodes[nodeCount].transform.position, step);
+
+        if ((Vector2)transform.position == (Vector2)nodes[nodeCount].transform.position)
+        {
+            if (nodeCount + 1 == nodes.Count)
+            {
+                nodeCount = 0;
+            }
+            else
+            {
+                nodeCount++;
+            }
+        }
+        Debug.Log(transform.position + " " + nodes[nodeCount].transform.position);
     }
 }
